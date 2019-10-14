e60e2ea7e0caf6ecb154477cef49f146de46c56f
Макс Гордійчук <user@Makss-MacBook-Pro.local>
2019-10-14 20:10:25 +0300

    Create ls
    exit
    ))

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from parsing_olx.pars_olx import *
 token = '962645516:AAFH_RuwvFilI_bP36TOkwKU66NbdeoBZWY'
 BASE_URL = 'https://api.telegram.org/bot' + token + '/'
 
+last_update_id = 0
 
 def get_html():
 	url = BASE_URL + 'getUpdates'
@@ -17,15 +18,23 @@ def get_html():
 def get_message():
 	data = get_html()
 
-	chat_id = data.get('result')[-1].get('message').get('chat').get('id')
-	text = data.get('result')[-1].get('message').get('text')
+	last_object = data.get('result')[-1]
+	current_update_id = last_object['update_id']
 
-	message = {
-		'chat_id': chat_id,
-		'text': text
-	}
+	global last_update_id
+	if last_update_id != current_update_id:
+		last_update_id = current_update_id
 
-	return message
+		chat_id = last_object.get('message').get('chat').get('id')
+		text = last_object.get('message').get('text')
+
+		message = {
+			'chat_id': chat_id,
+			'text': text
+		}
+
+		return message
+	return None
 
 
 def send_message(chat_id, text='Wait a second please...'):
@@ -41,13 +50,17 @@ def send_document(chat_id):
 
 
 def main():
-	data = get_message()
-	chat_id = data.get('chat_id')
-
-	if 'квартири' in data.get('text'):
-		advertisements_apartments = parse_olx(base_url, headers)
-		files_writer(advertisements_apartments)
-		send_document(chat_id)
+	while True:
+		answer = get_message()
+		if answer != None:
+			chat_id = answer.get('chat_id')
+			if 'квартири' in answer.get('text'):
+				advertisements_apartments = parse_olx(base_url, headers)
+				files_writer(advertisements_apartments)
+				send_message(chat_id, 'Дані по аренді квартир з olx.ua')
+				send_document(chat_id)
+			else:
+				continue
 
 
 if __name__ == '__main__':
